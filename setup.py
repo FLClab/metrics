920a5386c2ddcf06182e42ef692f48e78dbac628
antho214 <34163910+antho214@users.noreply.github.com>
2022-02-25 08:31:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from distutils.core import setup
 setup(
     name="metrics",
     version="0.1",
+    description="A metrics package",
     author="AB",
+    url="https://github.com/FLClab/metrics",
+    packages=["metrics"],
 )
-
